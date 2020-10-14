d6dfcb5d0372282ddcc38ef964ddb00c31d16f8b
Артемий <st076453@student.spbu.ru>
2020-10-14 23:02:01 +0300

    Попытка решить шестую

--- src/MassiveAssociation/Testesting.java
+++ src/MassiveAssociation/Testesting.java
@@ -7,5 +7,9 @@ public class Testesting {
         System.out.println(tr.Trans(s));
         String txt = "распокупочка";
         System.out.println(tr.Trans(txt));
+        Pereschet pr = new Pereschet();
+        System.out.println(pr.Schet(s));
+        Position ps = new Position();
+        System.out.println(ps.Pose(s));
     }
 }
