ef3affe062690e99dbb323e4dc3c654faa9b5c0c
Артемий <st076453@student.spbu.ru>
2021-03-10 00:17:56 +0300

    ДОБАВИЛИ КНОПКУ!!!

--- src/FXGraphica/src/Examples/JavaFXHelloWorld.java
+++ src/FXGraphica/src/Examples/JavaFXHelloWorld.java
@@ -1,4 +1,25 @@
 package Examples;
 
-public class JavaFXHelloWorld {
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
+public class JavaFXHelloWorld extends Application {
+    @Override
+    public void start(Stage mainstage) throws Exception {
+        mainstage.setTitle("Hollo from JavaFX!");
+
+        Parent button = initInterface();
+        mainstage.setScene(new Scene(button, 640, 480));
+
+        mainstage.show();
+    }
+
+    private Button initInterface(){
+        Button pressMe = new Button("Нажми на меня");
+        pressMe.setOnAction(event -> System.out.println("~БУУ~"));
+        return pressMe;
+    }
 }
---
+++ src/FXGraphica/src/module-info.java
@@ -0,0 +1,6 @@
+module Examples{
+    requires javafx.base;
+    requires javafx.controls;
+
+    opens Examples;
+}
